a9300daef3a167fea0c9eb8d327eaaec3137f70a
ryansoe <125227157+ryansoe@users.noreply.github.com>
2024-10-01 22:01:08 +0000

    changing hello

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "hola! aaaaaaaaaaaaa"; 
+  char hello2[] = { 73, 102, 109, 109, 112, 34, 0 };
+
+  puts(hello);
+  puts(hello2);
+
+  return 0;
+}
